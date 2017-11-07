62a5fbb9d83c32e5c44a27816898f823cece4c8e
GoodlyThink <goodlythink@gmail.com>
2017-11-07 11:30:58 +0700

    Redux Basic 1

--- web/Thor/Thor.js
+++ web/Thor/Thor.js
@@ -1,13 +1,23 @@
 import React from 'react'
 
 class Thor extends React.Component{
+    state = { count: 0 }
     render() {
         return (
             <div>
                 <h1>THOR</h1>
                 <div>As the Norse God of thunder and lightning, Thor wields one of the greatest weapons ever made, the enchanted hammer Mjolnir. While others have described Thor as an over-muscled, oafish imbecile, he's quite smart and compassionate. He's self-assured, and he would never, ever stop fighting for a worthwhile cause</div>
+                <button onClick={this.handleClick}>count {this.state.count}</button>
             </div>
         )
+    }
+
+    handleClick = () => {
+        this.setState(s => {
+            return {
+                count: s.count + 1
+            }    
+        })
     }
 }
 
--- web/index.js
+++ web/index.js
@@ -2,7 +2,25 @@ import './index.scss';
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { match, Router, browserHistory } from 'react-router'
+import { createStore } from 'redux'
 import routes from './routes'
+
+// (state, action) => state
+function counter(state = 0, action) {
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1
+        case 'DECREMETN':
+            return state - 1
+        default:
+            return state
+    }
+}
+
+const store = createStore(counter)
+store.subscribe(() => { console.log(store.getState()) })
+console.log(store.getState())
+store.dispatch({ type: 'INCREMENT' })
 
 match(
     { history: browserHistory, routes },
