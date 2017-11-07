0d0147b5bde5f0713a50e17ea09266c6c31b405c
GoodlyThink <goodlythink@gmail.com>
2017-11-07 11:27:13 +0700

    test fetch data

--- web/Home/Home.js
+++ web/Home/Home.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import PostList from '../Posts/PostList'
 
 class Home extends React.Component {
+    state = { data: null, isLoading: false }
+    componentDidMount() {
+        this.setState({ isLoading: true })
+        fetch('https://jsonplaceholder.typicode.com/posts?userId=1')
+            .then(d => d.json())
+            .then(response => this.setState({ data: response, isLoading: false }))
+    }
+
     render() {
+        const { isLoading, data } = this.state
         return (
             <div>
                 <h1>Lastes Posts</h1>
-                <div>
-                    <div>Post 1</div>
-                    <div>Post 2</div>
-                </div>
+                {isLoading && <div>Loading...</div>}
+                <PostList data={data}/>
             </div>
         )
     }
---
+++ web/Posts/PostList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Post from './Post'
+
+class PostList extends React.Component {
+    render() {
+        const { data } = this.props
+        return (
+            <div>
+                {data && data.map(e => {
+                    return (
+                        <Post data={e} key={`post.${e.id}`}/>
+                    )
+                })}
+            </div>
+        )
+    }
+}
+
+export default PostList
