f639cc0eed09bc73d2adabcbb823074ed51bd706
GoodlyThink <goodlythink@gmail.com>
2017-11-07 15:21:08 +0700

    Success ReduxThunk

--- web/actions.js
+++ web/actions.js
@@ -1,4 +1,14 @@
 export function loadPosts() {
+    return (dispatch, getState) => {
+        const { posts } = getState()
+        if (posts.data != null) {
+            return
+        }
+        return dispatch(fetchPosts())
+    }
+}
+
+function fetchPosts() {
     return {
         type: 'LOAD_POSTS',
         payload: fetch('https://jsonplaceholder.typicode.com/posts?userId=1')
--- web/configureStore.js
+++ web/configureStore.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from 'redux'
 import promiseMiddleware from 'redux-promise-middleware'
 import reducers from './reducers'
+import thunk from 'redux-thunk'
 
 export default function configureStore() {
     const store = createStore(
         reducers,
-        applyMiddleware(promiseMiddleware())
+        applyMiddleware(thunk, promiseMiddleware())
     )
     return store
 }
